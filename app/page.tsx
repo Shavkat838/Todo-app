079df1ded709ca5be66d9af789ad18ec8caf4944
Shavkat838 <boltayevshavkat216@gmail.com>
2025-12-22 18:35:22 +0500

    dynamik auto

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import prisma from "@/lib/db";
 import Header from "./_header/header";
 
@@ -6,22 +7,19 @@ import Body, { Todo } from "./Components/Body";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
-export default async  function Home() {
-
-const user= await currentUser()
-if(!user){
-  redirect("/sign-in")
-  return
-} 
-
-const data:Todo[]=await prisma.todos.findMany({
-  where:{
-    userId:user.id
+export default async function Home() {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+    return;
   }
-})
- 
- 
+
+  const data: Todo[] = await prisma.todos.findMany({
+    where: {
+      userId: user.id,
+    },
+  });
+
   return (
     <div className="max-w-[1220px] w-full px-[16px] min-h-screen border-2 pt-1 border-[#1F1F1F] rounded-[15px] bg-neutral-900">
       <Header />
